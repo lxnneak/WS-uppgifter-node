5f1d2ad8c66e7f51d5d736ed3364b49ea54e1d39
lxnneak <lienlinneakarlsson@gmail.com>
2025-09-08 10:10:07 +0200

    Add: Connection to Mongodb

---
+++ src/db/database.ts
@@ -0,0 +1,46 @@
+import { Db, MongoClient, ServerApiVersion } from "mongodb"
+import { validateSecret } from "../security/validateEnv.js"
+
+// Runtime-validation
+const uri: string = validateSecret(process.env.DB_CONNECTION_STRING)
+const dbName: string = validateSecret(process.env.DB_NAME)
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+let isConnected = false
+
+/*
+ * Attempts to connect to database
+ * Returns promise
+ * Throws error if failed
+ */
+export async function runDB(): Promise<void> {
+  if (isConnected) return // prevents duplicate connects
+
+  try {
+    await client.connect()
+    await client.db("admin").command({ ping: 1 })
+    isConnected = true
+    console.log("Db is up and running")
+  } catch (err) {
+    console.error(err)
+    throw err
+  }
+}
+
+export function getDB(): Db {
+  if (!isConnected) {
+    throw new Error("Tried to access DB before connecting")
+  }
+  return client.db(dbName)
+}
+
+export async function closeDB(): Promise<void> {
+  await client.close()
+  isConnected = false
+  console.log("Mongodb connection closed")
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,51 @@
 import express from "express"
+import "dotenv/config"
+import { closeDB, getDB, runDB } from "./db/database.js"
+import type { Db } from "mongodb"
 
 const app = express()
-const port: number = 3000
+const port: number = Number(process.env.PORT) || 3000
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello world!")
 })
 
 // Start server on Port Variable
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+// app.listen(port, "0.0.0.0", () => {
+//  console.log(`Listening on port ${port}`)
+// })
+
+async function startServer() {
+  try {
+    await runDB()
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}`)
+      console.log(`Start the app: http://localhost:${port}`)
+    })
+    process.on("SIGINT", async () => {
+      console.log("Cleaning up...")
+      await closeDB()
+      process.exit(0)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+startServer()
+
+app.get("/api/v1/database/comments/:username", async (req, res) => {
+  const db: Db = getDB()
+  const result = await db
+    .collection("comments")
+    .find({ name: req.params.username })
+    .limit(25)
+    .toArray()
+
+  if (result.length === 0) {
+    res.status(404).send({ message: "Nothing was found" })
+    return
+  }
+
+  res.send(result)
 })
